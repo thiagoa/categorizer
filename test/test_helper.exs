0e8fdebcc4131cb700efd5b9025311beff0f8fe0
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-04-20 00:44:36 -0300

    Setup test database and reset DB state between tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,16 +4,26 @@ defmodule DatabaseCase do
   defmacro __using__(_opts) do
     quote do
       use ExUnit.Case
+      use Categorizer.Database
+      alias Categorizer.Repo
 
       setup_all do
         Amnesia.Schema.create
         Amnesia.start
+
+        on_exit fn ->
+          Amnesia.stop
+          Amnesia.Schema.destroy
+        end
+
+        :ok
+      end
+
+      setup do
         Categorizer.Database.create!
 
         on_exit fn ->
           Categorizer.Database.destroy
-          Amnesia.stop
-          Amnesia.Schema.destroy
         end
 
         :ok
